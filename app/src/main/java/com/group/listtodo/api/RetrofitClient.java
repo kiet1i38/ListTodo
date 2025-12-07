e48f45be319d66f525577c727387fbaedad7c1d9
kiet1i38 <kietchaukg@gmail.com>
2025-12-08 02:13:54 +0700

    Update RetrofitClient.java

--- app/src/main/java/com/group/listtodo/api/RetrofitClient.java
+++ app/src/main/java/com/group/listtodo/api/RetrofitClient.java
@@ -4,7 +4,6 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    // 10.0.2.2 là địa chỉ localhost của máy tính khi chạy trên Android Emulator
     private static final String BASE_URL = "http://10.0.2.2:5000/";
     private static Retrofit retrofit = null;
 
